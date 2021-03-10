6f97f05bba128975003d040d67c722c214109f01
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-03-10 08:55:50 +0800

    add adapter design pattern source file

---
+++ adapter/adapter.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+#include <boost/algorithm/string.hpp>
+
+
+class String
+{
+    public:
+        std::string s;
+
+        String(const std::string & s) : s(s)
+        {
+            ;
+        }
+
+        ~String() {}
+        
+        String to_lower_copy() const
+        {
+            return {boost::}
+        }
+
+};
+int main()
+{
+    ;
+
+    return 0;
+}
