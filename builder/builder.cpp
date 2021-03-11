541b11c4b83282e71d82212221b2f0a2a921532e
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-03-11 09:48:03 +0800

    style: update source file

--- builder/builder.cpp
+++ builder/builder.cpp
@@ -102,7 +102,7 @@ int main(int argc, char ** argv)
     std::cout << output << std::endl;
 
     // <ul><li>hello</li><li>world</li></ul>
-    std::cout << "ordinary method 2" << "\n=================" << std::endl;
+    std::cout << "\nordinary method 2" << "\n=================" << std::endl;
     std::string words[] = {"hello", "world"};   // Passing in initializer list {}
     std::ostringstream oss;
     // Opening
@@ -115,12 +115,12 @@ int main(int argc, char ** argv)
     std::cout << oss.str() << std::endl;
 
     // Builder Design Pattern
-    std::cout << "Builder Design Pattern 1" << "\n========================" << std::endl;
+    std::cout << "\nBuilder Design Pattern 1" << "\n========================" << std::endl;
     HtmlBuilder builder("ui");
     builder.add_child("li", "hello").add_child("li", "world");
     std::cout << builder.str() << std::endl;
 
-    std::cout << "Builder Design Pattern 2" << "\n========================" << std::endl;
+    std::cout << "\nBuilder Design Pattern 2" << "\n========================" << std::endl;
     HtmlElement e = HtmlElement::build("ui")->add_child("li", "hello").add_child("li", "world");
     std::cout << e.str() << std::endl;
 
